35826390b5e0de1bfcb30106f5573963b70e3e7c
Ope2b <rakzeiqo@live.fr>
2021-11-18 14:24:46 +0100

    2nd commit

---
+++ project-pierre/src/main/java/com/project/m2dfs/projectpierre/controller/FootballRestController.java
@@ -0,0 +1,14 @@
+package com.project.m2dfs.projectpierre.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FootballRestController {
+
+    @GetMapping("/result")
+    public String myMethod(){
+        return "first commit";
+    }
+}
