ae4233e253622509d69933c447c372a20bf473ec
Ope2b <rakzeiqo@live.fr>
2021-11-18 13:55:36 +0100

    create first microservice

---
+++ project-pierre/src/main/java/com/project/m2dfs/projectpierre/ProjectPierreApplication.java
@@ -0,0 +1,13 @@
+package com.project.m2dfs.projectpierre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPierreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPierreApplication.class, args);
+	}
+
+}
